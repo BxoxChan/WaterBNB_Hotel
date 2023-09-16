dbe4505df9df126f01352a38db1b627d3ac3a06d
1234bhaskar <bhaskar2021chand@gmail.com>
2023-09-16 22:06:40 +0530

    making Login and Register form active

--- client/Frontend/src/App.jsx
+++ client/Frontend/src/App.jsx
@@ -4,10 +4,13 @@ import { Properties } from './pages/Properties'
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
 import PropertyPage from './pages/PropertyPage'
 import { Register } from './pages/Register'
+import {Toaster} from 'react-hot-toast';
+import Login from './pages/Login'
 
 const App = () => {
   return (
     <div className='font-Raleway'>
+    <Toaster position="bottom-center" toastOptions={{duration:5000}}/>
       <Router>
       <Routes>
         <Route path='/' element={ <Home/>}/>
@@ -16,11 +19,12 @@ const App = () => {
           <Route path='/properties/:id' element={<PropertyPage/>}/>
         </Route>
         <Route path='/register' element={<Register/>}/>
-
-
+        <Route path='/login' element={<Login/>}/>
       </Routes>
       </Router>
+      
     </div>
+    
   )
 }
 
--- client/Frontend/src/pages/Login.jsx
+++ client/Frontend/src/pages/Login.jsx
@@ -1,47 +1,38 @@
 import React, { useState } from 'react'
 import { userRequest } from '../utils/requestMethods';
 import toast from 'react-hot-toast';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 
 
-export const Register = () => {
-    //  const[name,setName]=useState('');
-    //  const[email,setEmail]=useState('');
-    //  const[password,setPassword]=useState('');
-     const [data,setData]=useState({
-         username:'',
-         email:'',
-         password:''
-     })
+const Login = () => {
+    const [data,setData]=useState({
+        email:'',
+        password:''
+    });
+    const navigate=useNavigate();
     const formhandler=async(e)=>{
-     e.preventDefault();
-
-     const{username,email,password}=data;
-    try{
-
-        const {data}=await userRequest.post("/register",{
-            username,email,password});
-            if(data.error){
-                toast.error(data.error);
-            }else{
-                setData({})
-                toast.success('User Registered Succesfully');
-            }
+        e.preventDefault();
+        const {email,password}=data;
+        try{
+         const {data}=await userRequest.post("/login",{
+            email,password
+         })   
+         if(data.error){
+            toast.error(data.error)
+         }else{
+            setData({})
+            navigate('/');
+         }
         }catch(e){
           console.log(e);
         }
     }
   return (
-    <div className='h-screen w-full flex justify-center items-center'>
+   <div className='h-screen w-full flex justify-center items-center'>
        <div className='w-1/3 flex flex-col items-center border  rounded-md py-5'>
-        <h1 className='text-4xl my-5 '>Register</h1>
+        <h1 className='text-4xl my-5 '>Login</h1>
         <form action="" onSubmit={formhandler}>
-            <div className='flex flex-col text-2xl my-2'>
-            <label className='' htmlFor="">Name</label>
-            <input className='border rounded-md outline-none' type="text" value={data.username} onChange={
-                (e)=> setData({...data,username:e.target.value})} required/>
-            </div>
             <div  className='flex flex-col text-2xl my-2'>
             <label htmlFor="" className=''>Email</label>
             <input className='border rounded-md outline-none' type="email" value={data.email} onChange={
@@ -58,8 +49,10 @@ export const Register = () => {
             <button className='rounded bg-blue1 px-4 py-2'>Register</button>
             </div>
         </form>
-        <p>if already registerd,<span className='font-bold'><Link to={"/login"}>Login</Link></span></p>
+        <p>if already registerd,<span className='font-bold'>Login</span></p>
        </div>
     </div>
   )
 }
+
+export default Login
