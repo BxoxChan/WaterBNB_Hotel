6ced91fb4f81ac8a288c31652ada432a95f4340c
1234bhaskar <bhaskar2021chand@gmail.com>
2023-09-17 17:13:38 +0530

    Updated Logout Route

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -114,11 +114,11 @@ router.post("/login", async (req, res) => {
 // });
 
 router.post("/logout", (req, res) => {
-  res.cookie("token", "", {
+  res.cookie("token", null, {
     httpOnly: true,
-    expires: new Date(0),
+    expires: new Date(Date.now()),
   });
-  res.status(200).json({ message: "Successfully loged out" });
+  res.status(200).json({ success: true, message: "Successfully loged out" });
 });
 
 export { router as authroute };
