5ef1ba5cda8e2850dd9cd5003affe4ea3d51da24
1234bhaskar <bhaskar2021chand@gmail.com>
2023-09-30 19:45:58 +0530

    SearchBar Component Updated using Context

--- client/Frontend/src/Components/SearchBar.jsx
+++ client/Frontend/src/Components/SearchBar.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import SearchIcon from '@mui/icons-material/Search';
+import {UserContext} from '../../context/UserContext'
 const SearchBar = () => {
+  const {filter,setFilter}=useContext(UserContext);
   return (
     <div className='flex justify-center my-8 '>
         <div className='border-2 border-black w-1/2 flex p-1 rounded-md'>
         <SearchIcon/>
-        <input type="search" className=' w-full outline-none ' />
+        <input type="search" placeholder='Search by city' className=' w-full outline-none ' value={filter} onChange={(e)=>setFilter(e.target.value)}/>
         </div>
     </div>
   )
