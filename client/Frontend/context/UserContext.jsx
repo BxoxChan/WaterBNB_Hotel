4169a0d77bfd638ef17142ce33d6af70c76eb0f8
1234bhaskar <bhaskar2021chand@gmail.com>
2023-09-30 19:39:15 +0530

    creating Context state for Search Filter

--- client/Frontend/context/UserContext.jsx
+++ client/Frontend/context/UserContext.jsx
@@ -3,9 +3,10 @@ export const UserContext = createContext({});
 
 export const UserContextProvider = ({ children }) => {
   const [user, setUser] = useState("");
+  const [filter,setFilter]=useState("")
 
   return (
-    <UserContext.Provider value={{ user, setUser }}>
+    <UserContext.Provider value={{ user, setUser,filter,setFilter }}>
       {children}
     </UserContext.Provider>
   );
