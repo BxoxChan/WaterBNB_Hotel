020792f2c3f16f474c5f0c7bf1a4d4be3e5fa53a
1234bhaskar <bhaskar2021chand@gmail.com>
2023-09-30 19:43:48 +0530

    Making filter for SearchBar{useContext & filter()}

--- client/Frontend/src/Components/PropertyComponent/Properties.jsx
+++ client/Frontend/src/Components/PropertyComponent/Properties.jsx
@@ -1,16 +1,17 @@
-import React, { useEffect,useState } from 'react'
+import React, { useContext, useEffect,useState } from 'react'
 import PropertCard from './PropertCard'
 import axios from 'axios';
+import {UserContext} from '../../../context/UserContext'
 
 
 function Properties() {
-
+    const {filter}=useContext(UserContext);
     const [property,setProperty]=useState([]);
   useEffect(()=>{
     const fetchData=async()=>{
         const data= await axios.get("http://localhost:5000/app/residence/Residence").then((data)=>{
             const properties=data.data;
-            console.log(properties);
+           // console.log(properties);
             setProperty(properties);
         }).catch((e)=>{
             console.log(e);
@@ -21,10 +22,9 @@ function Properties() {
   return (
     <div className='min-h-screen mx-10 grid grid-cols-3 gap-5 '>
       {
-          property.map((prop)=>{
+          property.filter((data)=>data.city.toLowerCase().includes(filter.toLowerCase())).map((prop)=>{
             return(
             <PropertCard 
-             
              key={prop._id}
              id={prop._id}
              name={prop.title} 
