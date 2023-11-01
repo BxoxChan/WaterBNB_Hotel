ba23ccd804fb9b7cb9a2ac787a2198b724850239
1234bhaskar <bhaskar2021chand@gmail.com>
2023-11-01 14:26:44 +0530

    Booking Schema Created

---
+++ api/model/Booking.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const BookingSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  place: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Place",
+    required: true,
+  },
+  checkIn: {
+    type: Date,
+    required: true,
+  },
+  checkOut: {
+    type: Date,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  guests: {
+    type: Number,
+    required: true,
+  },
+  totalPrice: {
+    type: Number,
+  },
+  numberOfNights: {
+    type: Number,
+  },
+  username: {
+    type: String,
+  },
+});
+
+export const Booking = mongoose.model("Booking", BookingSchema);
