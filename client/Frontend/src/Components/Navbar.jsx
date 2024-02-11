a635089fe95ff2075cd06cdaa5d2de7f8628653e
1234bhaskar <bhaskar2021chand@gmail.com>
2024-02-11 11:24:48 +0530

    getting user from local storage else redirecting to register page

--- client/Frontend/src/Components/Navbar.jsx
+++ client/Frontend/src/Components/Navbar.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useContext } from 'react';
 import { UserContext } from '../../context/UserContext';
 
 const Navbar = () => {
-  const {user}=useContext(UserContext);
+  //const {user}=useContext(UserContext);
+  const user = localStorage.getItem("auth-User")
+  //const [persist,setPersist]=useState(null);
   return (
     <>
         <nav className='flex justify-between  list-none w-1/2 px-5 items-center text-xl '>
-          <li>
+          <li className='hover:border-b-2 hover:border-black visited:border-b-2 visited:border-black'>
           <Link to={"/properties"}>Properties</Link>
           </li>
-          <li>About Us</li>
-          <li>Contacts</li>
+          <li className='hover:border-b-2 hover:border-black'>About Us</li>
+          <li className='hover:border-b-2 hover:border-black'>Contacts</li>
         </nav>
     
         <div className='w-1/4 flex justify-center items-center'>
           {
-           (user.userDetails !=null)?
+           (user !=null)?
             <Link to={"/profile"}>
               <button className='text-white bg-pink-500 p-2 px-4 text-xl rounded-md font-semibold pl-5'>Profile</button>
             </Link>
