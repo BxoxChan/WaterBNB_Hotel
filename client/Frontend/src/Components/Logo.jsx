24338cd22bbda41f61fcc2f40150fdcb06c73de0
1234bhaskar <bhaskar2021chand@gmail.com>
2024-02-11 11:15:03 +0530

    added home page link to LOGO

--- client/Frontend/src/Components/Logo.jsx
+++ client/Frontend/src/Components/Logo.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Logo = () => {
+  const navigate=useNavigate();
   return (
-        <div className='text-5xl font-bold text-blue1 sm:w-1/4 sm:ml-10 py-2 '>
+        <div className='text-5xl font-bold text-blue1 sm:w-1/4 sm:ml-10 py-2 cursor-pointer ' onClick={()=>{navigate('/')}}>
         Logo
        </div>
   )
