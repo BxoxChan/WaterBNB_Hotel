de5f890a47cdb11cbc2a1235d97552a830080107
1234bhaskar <bhaskar2021chand@gmail.com>
2024-02-11 11:19:14 +0530

    Connected Booking Comp with resp. route(Booking Functionality)

--- client/Frontend/src/Components/Booking.jsx
+++ client/Frontend/src/Components/Booking.jsx
@@ -3,18 +3,23 @@ import {differenceInCalendarDays} from "date-fns"
 import { UserContext } from '../../context/UserContext';
 import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
+import  {bookingRequest}  from '../utils/requestMethods.js';
+import axios from 'axios';
 
 const Booking = (places) => {
-      const {user}=useContext(UserContext);
+      //const {user}=useContext(UserContext);
+        const user = JSON.parse(localStorage.getItem('auth-User'));
        const navigate=useNavigate();
     const [guests,setGuests]=useState(1);
     const [phnumber,setPhnumber]=useState('');
-    const [userName,setUserName]=useState(user?user.userDetails.username:'');
+    //const [userName,setUserName]=useState(user?user.userDetails.username:'');
+    const [userName,setUserName]=useState(user?user.username:'');
     const [checkInDate,setcheckInDate]=useState(
         new Date().toISOString().slice(0,10)
     );
     const [checkOutDate,setcheckOutDate]=useState("")
-  const [numberOfNights, setNumberOfNights] = useState(0);    
+  const [numberOfNights, setNumberOfNights] = useState(0);
+  //const [booking,setBooking]=useState({});    
     // const [bookingData,setBookingData]=useState({
     //     guests:1,
     //     phnumber:'',
@@ -28,23 +33,57 @@ const Booking = (places) => {
         differenceInCalendarDays(new Date(checkOutDate),new Date(checkInDate))
     )
    },[checkOutDate,checkInDate]);
-   
-   const bookingHandler=(e)=>{
-    e.preventDefault();
-    const addBooking =()=>
-    {
-        console.log({
-            property_id:places.id,
-            userName,phnumber,guests,checkInDate,checkOutDate,numberOfNights,
-            totalPrice:numberOfNights * places.price,
-        });
-    }
-    addBooking();
-   }
 
    const notloginUser=()=>{
-toast.error('Please Login for Booking');
+    toast.error('Please Login for Booking');
     navigate('/login')
+   }
+
+   const bookingHandler=(e)=>{
+    e.preventDefault();
+    const addBooking = async()=>
+    {
+
+      try{
+       const booking= await bookingRequest.post("/",{
+            place:places.id,
+            username:userName,
+            user:user._id,
+            phone:phnumber,
+            guests,
+            checkIn:checkInDate,
+            checkOut:checkOutDate,
+            numberOfNights,
+            totalPrice:numberOfNights * guests * places.price
+          }
+        ) ;
+         
+          toast.success("Booked successfully");
+        //   console.log({
+        //     property_id:places.id,
+        //     user:user._id,
+        //     userName,phnumber,guests,checkInDate,checkOutDate,numberOfNights,
+        //     totalPrice:numberOfNights * places.price,
+        // });
+         }
+        //console.log({booking});
+        //setBooking({});
+        
+          
+      catch(error){
+        toast.error(" not booked!")
+          console.log(error);
+      }
+    //   }finally{
+    //     //setBooking({});
+    //   }
+        // console.log({
+        //     property_id:places.id,
+        //     userName,phnumber,guests,checkInDate,checkOutDate,numberOfNights,
+        //     totalPrice:numberOfNights * places.price,
+        // });
+    }
+    addBooking();
    }
   return (
     <div className='mx-15  px-10 rounded-md shadow-shadowTwo'>
@@ -84,7 +123,7 @@ toast.error('Please Login for Booking');
             </div>
             <div className='flex flex-col mt-2'>
                <label htmlFor="" className='font-bold text-2xl'>Phone number:</label> 
-               <input  type="number" className='border border-gray-200 p-2 outline-none'  value={phnumber} onChange={(e)=>{
+               <input  type="number"  className='border border-gray-200 p-2 outline-none'  value={phnumber} onChange={(e)=>{
                 setPhnumber(e.target.value)
                }}/>
             </div>
