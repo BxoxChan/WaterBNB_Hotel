d9406e20274e8732fac252359d60c4b45a07b5ef
1234bhaskar <bhaskar2021chand@gmail.com>
2024-03-03 19:28:04 +0530

    custom css Added

--- client/Frontend/tailwind.config.js
+++ client/Frontend/tailwind.config.js
@@ -9,12 +9,19 @@ export default {
       boxShadow: {
         shadowCust: "19px 21px 25px -17px rgba(0,0,0,0.25)",
         shadowTwo: "2px 13px 25px -7px rgba(0,0,0,0.28)",
+        Login: "0 0px 10px 2px whitesmoke",
       },
       fontFamily: {
         Montserrat: ["Montserrat", "sans-serif"],
         Ubuntu: ["Ubuntu", "sans-serif"],
         Raleway: ["Raleway", "sans-serif"],
       },
+      backgroundColor: {
+        //Cust: "bg-gradient-to-r from-violet-600 via-violet-600 to-blue1",
+        Cust: "from-violet-600 to-indigo-600",
+        // Cust: "linear-gradient(90deg, rgba(240,2,246,1) 0%, rgba(252,5,255,1) 33%, rgba(6,158,250,1) 66%)",
+      },
+      boxShadowColor: {},
     },
   },
   plugins: [],
