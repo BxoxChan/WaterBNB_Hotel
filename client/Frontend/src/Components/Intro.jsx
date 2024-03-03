a374dea6f7aca965e5b1ada347ae22333e0a9a3f
1234bhaskar <bhaskar2021chand@gmail.com>
2024-03-03 19:29:01 +0530

    Ui improved of main page

--- client/Frontend/src/Components/Intro.jsx
+++ client/Frontend/src/Components/Intro.jsx
@@ -5,24 +5,24 @@ import { useNavigate } from 'react-router-dom'
 export const Intro = () => {
   const navigate=useNavigate();
   return (
-    <div className="overflow-x-hidden ">
-    <div className='relative'>
-      <div className='flex justify-center'>
-      <img src={image1} alt="" className='w-4/5 rounded-md brightness-50'/>
+    <div className=" mt-5">
+    <div className='flex w-full '>
+      <div className='w-3/5 flex-1 p-2'>
+      <img src={image1} alt="" className='w-full rounded-md brightness'/>
       </div>
-    <div className='absolute top-0 left-36 text-white mt-24'>
+
+    <div className=' text-black  flex-1 pl-5'>
        <span className='text-8xl mb-2'>
-        <p className='text-white font-bold text-6xl'>Enjoy Your Vecations in our</p>
-       <p className='text-blue1 font-bold'>luxurious Homes</p>
+        <p className='text-black font-bold text-6xl'>Enjoy Your Vecations in our</p>
+       <p className='text-blue1 font-bold '>luxurious Homes</p>
        </span>
        
-       <p className='w-1/2 mt-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem nobis reprehenderit quibusdam animi expedita nostrum aperiam esse, incidunt omnis laudantium modi nesciunt facere vero ad et repellat illo, perferendis ipsa.</p>
+       <p className='w-4/5 mt-2 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem nobis reprehenderit quibusdam animi expedita nostrum aperiam esse, incidunt omnis laudantium modi nesciunt facere vero ad et repellat illo, perferendis ipsa.</p>
 
-       <div className='mt-10'>
-        <button className='text-white bg-blue-600 p-2 px-4 text-2xl rounded-md font-semibold' onClick={()=>{navigate('/properties')}}>View Property</button>
-       </div>
-
+      <button className='text-black bg-blue-600 p-2 px-4 text-2xl rounded-md font-semibold my-2' onClick={()=>{navigate('/properties')}}>View Property</button>
+       
     </div>
+
     </div>
     </div>
   )
